0c3998c5d951992e894d652f1e7d79cbdffefb2b
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2014-07-05 15:15:28 +0900

    add register name of debug UART.

--- src/net/katsuster/semu/UART.java
+++ src/net/katsuster/semu/UART.java
@@ -6,13 +6,36 @@ package net.katsuster.semu;
  * @author katsuhiro
  */
 public class UART extends SlaveCore64 {
+    //データ幅（バイト単位）
+    public static final int LEN_WORD = 4;
+    //データ幅（ビット単位）
+    public static final int LEN_WORD_BITS = LEN_WORD * 8;
+
+    public static final int REG_UARTDR        = 0x000;
+    public static final int REG_UARTRSR       = 0x004;
+    public static final int REG_UARTFR        = 0x018;
+    public static final int REG_UARTILPR      = 0x020;
+    public static final int REG_UARTIBRD      = 0x024;
+    public static final int REG_UARTFBRD      = 0x028;
+    public static final int REG_UARTLCR_H     = 0x02c;
+    public static final int REG_UARTCR        = 0x030;
+    public static final int REG_UARTIFLS      = 0x034;
+    public static final int REG_UARTIMSC      = 0x038;
+    public static final int REG_UARTRIS       = 0x03c;
+    public static final int REG_UARTMIS       = 0x040;
+    public static final int REG_UARTICR       = 0x044;
+    public static final int REG_UARTDMACR     = 0x048;
+    public static final int REG_UARTPeriphID0 = 0xfe0;
+    public static final int REG_UARTPeriphID1 = 0xfe4;
+    public static final int REG_UARTPeriphID2 = 0xfe8;
+    public static final int REG_UARTPeriphID3 = 0xfec;
+    public static final int REG_UARTPCellID0  = 0xff0;
+    public static final int REG_UARTPCellID1  = 0xff4;
+    public static final int REG_UARTPCellID2  = 0xff8;
+    public static final int REG_UARTPCellID3  = 0xffc;
+
     public UART() {
 
-    }
-
-    public boolean tryAccess(long addr) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
     }
 
     @Override
@@ -22,20 +45,21 @@ public class UART extends SlaveCore64 {
 
     @Override
     public byte read8(long addr) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        long v = readWord(addr);
+
+        return (byte)readMasked(addr, v, LEN_WORD_BITS, 8);
     }
 
     @Override
     public short read16(long addr) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        long v = readWord(addr);
+
+        return (byte)readMasked(addr, v, LEN_WORD_BITS, 16);
     }
 
     @Override
     public int read32(long addr) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        return readWord(addr);
     }
 
     @Override
@@ -51,26 +75,82 @@ public class UART extends SlaveCore64 {
 
     @Override
     public void write8(long addr, byte data) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        long v = readWord(addr);
+        long w = writeMasked(addr, v, data, LEN_WORD_BITS, 8);
+
+        writeWord(addr, w);
     }
 
     @Override
     public void write16(long addr, short data) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        long v = readWord(addr);
+        long w = writeMasked(addr, v, data, LEN_WORD_BITS, 16);
 
+        writeWord(addr, w);
     }
 
     @Override
     public void write32(long addr, int data) {
-        //TODO: Not implemented
-        throw new IllegalArgumentException("Sorry, not implemented.");
+        long v = readWord(addr);
+        long w = writeMasked(addr, v, data, LEN_WORD_BITS, 32);
+
+        writeWord(addr, w);
     }
 
     @Override
     public void write64(long addr, long data) {
         //TODO: Not implemented
         throw new IllegalArgumentException("Sorry, not implemented.");
+    }
+
+    public boolean tryAccess(long addr) {
+        int regaddr;
+        int result;
+
+        regaddr = (int)(addr & getAddressMask(LEN_WORD_BITS));
+
+        switch (regaddr) {
+        case REG_UARTDR:
+        case REG_UARTFR:
+            return true;
+        default:
+            throw new IllegalArgumentException(String.format("Illegal address 0x%08x.",
+                    regaddr));
+        }
+    }
+
+    public int readWord(long addr) {
+        int regaddr;
+        int result;
+
+        regaddr = (int)(addr & getAddressMask(LEN_WORD_BITS));
+
+        switch (regaddr) {
+        case REG_UARTDR:
+            //TODO: Not implemented
+            throw new IllegalArgumentException("Sorry, not implemented.");
+            //break;
+        default:
+            throw new IllegalArgumentException(String.format("Illegal address 0x%08x.",
+                    regaddr));
+        }
+
+        //return result;
+    }
+
+    public void writeWord(long addr, long data) {
+        int regaddr;
+
+        regaddr = (int)(addr & getAddressMask(LEN_WORD_BITS));
+
+        switch (regaddr) {
+        case REG_UARTDR:
+            //TODO: Not implemented
+            throw new IllegalArgumentException("Sorry, not implemented.");
+            //break;
+        default:
+            throw new IllegalArgumentException(String.format("Illegal address 0x%08x.",
+                    regaddr));
+        }
     }
 }
