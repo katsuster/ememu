3c468b582acefe090138aca607f89e8e95a3d90a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2014-08-07 09:37:54 +0900

    remove PL111 registers from the color LCD controller.

--- src/net/katsuster/semu/LCDC.java
+++ src/net/katsuster/semu/LCDC.java
@@ -15,8 +15,8 @@ public class LCDC extends Controller64Reg32 {
     public static final int REG_LCDTiming3       = 0x00c;
     public static final int REG_LCDUPBASE        = 0x010;
     public static final int REG_LCDLPBASE        = 0x014;
-    public static final int REG_LCDControl       = 0x018;
-    public static final int REG_LCDIMSC          = 0x01c;
+    public static final int REG_LCDIMSC          = 0x018;
+    public static final int REG_LCDControl       = 0x01c;
     public static final int REG_LCDRIS           = 0x020;
     public static final int REG_LCDMIS           = 0x024;
     public static final int REG_LCDICR           = 0x028;
@@ -24,19 +24,6 @@ public class LCDC extends Controller64Reg32 {
     public static final int REG_LCDLPCURR        = 0x030;
 
     //0x200-0x3FC: LCDPalette
-    //0x800-0xBFC: CursorImage
-
-    public static final int REG_ClcdCrsrCtrl     = 0xc00;
-    public static final int REG_ClcdCrsrConfig   = 0xc04;
-    public static final int REG_ClcdCrsrPalette0 = 0xc08;
-    public static final int REG_ClcdCrsrPalette1 = 0xc0c;
-    public static final int REG_ClcdCrsrXY       = 0xc10;
-    public static final int REG_ClcdCrsrClip     = 0xc14;
-
-    public static final int REG_ClcdCrsrIMSC     = 0xc20;
-    public static final int REG_ClcdCrsrICR      = 0xc24;
-    public static final int REG_ClcdCrsrRIS      = 0xc28;
-    public static final int REG_ClcdCrsrMIS      = 0xc2c;
 
     public static final int REG_CLCDPERIPHID0    = 0xfe0;
     public static final int REG_CLCDPERIPHID1    = 0xfe4;
@@ -63,19 +50,6 @@ public class LCDC extends Controller64Reg32 {
         //addReg(REG_LCDLPCURR, "LCDLPCURR", X);
 
         //0x200-0x3FC: LCDPalette
-        //0x800-0xBFC: CursorImage
-
-        //addReg(REG_ClcdCrsrCtrl, "ClcdCrsrCtrl", 0x00000000);
-        //addReg(REG_ClcdCrsrConfig, "ClcdCrsrConfig", 0x00000000);
-        //addReg(REG_ClcdCrsrPalette0, "ClcdCrsrPalette0", 0x00000000);
-        //addReg(REG_ClcdCrsrPalette1, "ClcdCrsrPalette1", 0x00000000);
-        //addReg(REG_ClcdCrsrXY, "ClcdCrsrXY", 0x00000000);
-        //addReg(REG_ClcdCrsrClip, "ClcdCrsrClip", 0x00000000);
-
-        //addReg(REG_ClcdCrsrIMSC, "ClcdCrsrIMSC", 0x00000000);
-        //addReg(REG_ClcdCrsrICR, "ClcdCrsrICR", 0x00000000);
-        //addReg(REG_ClcdCrsrRIS, "ClcdCrsrRIS", 0x00000000);
-        //addReg(REG_ClcdCrsrMIS, "ClcdCrsrMIS", 0x00000000);
 
         addReg(REG_CLCDPERIPHID0, "CLCDPERIPHID0", 0x10);
         addReg(REG_CLCDPERIPHID1, "CLCDPERIPHID1", 0x11);
