2bbcb67af9213a88c7ff3d3bffe10dc33e539696
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2015-03-04 03:08:50 +0900

    tentative implements DualTimer core. Works fine on the HZ = 100Hz environment.

--- arm/src/net/katsuster/ememu/arm/DualTimer.java
+++ arm/src/net/katsuster/ememu/arm/DualTimer.java
@@ -13,8 +13,16 @@ package net.katsuster.ememu.arm;
 public class DualTimer extends Controller64Reg32
         implements INTSource {
     private INTDestination intDst = new NullINTDestination();
+    private int clock;
+    private boolean[] timerEn;
+    private boolean[] timerPeriodic;
+    private int[] intEnable;
+    private int[] prescale;
+    private boolean[] timerSize32;
+    private boolean[] oneshot;
     private int[] rawInt;
-    private int[] intMask;
+    private int[] loadValue;
+    private int[] currentValue;
 
     public static final int REG_Timer1Load     = 0x000;
     public static final int REG_Timer1Value    = 0x004;
@@ -42,11 +50,28 @@ public class DualTimer extends Controller64Reg32
     public static final int REG_TimerPCellID3  = 0xffc;
 
     /**
-     * タイマーを作成します。
+     * 1MHz 駆動のタイマーを作成します。
      */
     public DualTimer() {
+        this(1000000);
+    }
+
+    /**
+     * タイマーを作成します。
+     *
+     * @param ck タイマーを駆動するクロックの周波数
+     */
+    public DualTimer(int ck) {
+        clock = ck;
+        timerEn = new boolean[2];
+        timerPeriodic = new boolean[2];
+        prescale = new int[2];
+        timerSize32 = new boolean[2];
+        oneshot = new boolean[2];
         rawInt = new int[2];
-        intMask = new int[2];
+        intEnable = new int[2];
+        loadValue = new int[2];
+        currentValue = new int[2];
 
         addReg(REG_Timer1Load, "Timer1Load", 0x00000000);
         addReg(REG_Timer1Value, "Timer1Value", 0xffffffff);
@@ -72,27 +97,64 @@ public class DualTimer extends Controller64Reg32
         addReg(REG_TimerPCellID1, "TimerPCellID1", 0x000000f0);
         addReg(REG_TimerPCellID2, "TimerPCellID2", 0x00000005);
         addReg(REG_TimerPCellID3, "TimerPCellID3", 0x000000b1);
+
+        //コントロールレジスタの設定を反映する
+        updateControl(0, REG_Timer1Control, 0x20);
+        updateControl(1, REG_Timer2Control, 0x20);
     }
 
-    public void changeControl(int id, long regaddr, int val) {
-        boolean enabled = BitOp.getBit32(val, 7);
-        boolean periodic = BitOp.getBit32(val, 6);
+    /**
+     * コントロールレジスタの設定を反映する。
+     *
+     * @param id タイマー ID
+     * @param regaddr レジスタのアドレス
+     * @param val レジスタの値
+     */
+    public void updateControl(int id, long regaddr, int val) {
+        boolean en = BitOp.getBit32(val, 7);
+        boolean peri = BitOp.getBit32(val, 6);
         boolean inten = BitOp.getBit32(val, 5);
+        int pre = BitOp.getField32(val, 2, 2);
         boolean size32 = BitOp.getBit32(val, 1);
-        boolean oneshot = BitOp.getBit32(val, 0);
+        boolean one = BitOp.getBit32(val, 0);
 
         System.out.printf("Timer%dControl: 0x%x.\n", id + 1, val);
-        System.out.printf("  enabled : %b.\n", enabled);
-        System.out.printf("  periodic: %b.\n", periodic);
-        System.out.printf("  inten   : %b.\n", inten);
-        System.out.printf("  size32  : %b.\n", size32);
-        System.out.printf("  oneshot : %b.\n", oneshot);
+        System.out.printf("  timerEn      : %b\n", en);
+        System.out.printf("  timerPeriodic: %b\n", peri);
+        System.out.printf("  intEnable    : %b\n", inten);
+        System.out.printf("  timerPre     : %d\n", pre);
+        System.out.printf("  timerSize32  : %b\n", size32);
+        System.out.printf("  oneshot      : %b\n", one);
+
+        timerEn[id] = en;
+        timerPeriodic[id] = peri;
 
         if (inten) {
-            intMask[id] = 0x1;
+            intEnable[id] = 0x1;
         } else {
-            intMask[id] = 0x0;
+            intEnable[id] = 0x0;
         }
+
+        switch (pre) {
+        case 0:
+            //clock is divided by 1
+            prescale[id] = 1;
+            break;
+        case 1:
+            //clock is divided by 16
+            prescale[id] = 16;
+            break;
+        case 2:
+            //clock is divided by 256
+            prescale[id] = 256;
+            break;
+        case 3:
+            //undefined, ignored
+            break;
+        }
+
+        timerSize32[id] = size32;
+        oneshot[id] = one;
 
         super.writeWord(regaddr, val);
     }
@@ -105,6 +167,12 @@ public class DualTimer extends Controller64Reg32
         regaddr = (int)(addr & getAddressMask(LEN_WORD_BITS));
 
         switch (regaddr) {
+        case REG_Timer1Load:
+            result = super.readWord(regaddr);
+            break;
+        case REG_Timer1Value:
+            result = currentValue[0];
+            break;
         case REG_Timer1IntClr:
             //write only, ignored
             result = 0;
@@ -113,7 +181,16 @@ public class DualTimer extends Controller64Reg32
             result = rawInt[0];
             break;
         case REG_Timer1MIS:
-            result = rawInt[0] & intMask[0];
+            result = rawInt[0] & intEnable[0];
+            break;
+        case REG_Timer1BGLoad:
+            result = super.readWord(regaddr);
+            break;
+        case REG_Timer2Load:
+            result = super.readWord(regaddr);
+            break;
+        case REG_Timer2Value:
+            result = currentValue[1];
             break;
         case REG_Timer2IntClr:
             //write only, ignored
@@ -123,7 +200,10 @@ public class DualTimer extends Controller64Reg32
             result = rawInt[1];
             break;
         case REG_Timer2MIS:
-            result = rawInt[1] & intMask[1];
+            result = rawInt[1] & intEnable[1];
+            break;
+        case REG_Timer2BGLoad:
+            result = super.readWord(regaddr);
             break;
         default:
             result = super.readWord(regaddr);
@@ -139,56 +219,66 @@ public class DualTimer extends Controller64Reg32
 
         regaddr = (int) (addr & getAddressMask(LEN_WORD_BITS));
 
-        switch (regaddr) {
-        case REG_Timer1Load:
-            //TODO: not implemented
-            System.out.printf("Timer1Load: 0x%08x\n", data);
-            break;
-        case REG_Timer1Value:
-            //TODO: not implemented
-            System.out.printf("Timer1Value: 0x%08x\n", data);
-            break;
-        case REG_Timer1Control:
-            changeControl(0, regaddr, data);
-            break;
-        case REG_Timer1IntClr:
-            rawInt[0] &= ~data;
-            break;
-        case REG_Timer1RIS:
-        case REG_Timer1MIS:
-            //read only, ignored
-            break;
-        case REG_Timer2Load:
-            //TODO: not implemented
-            System.out.printf("Timer2Load: 0x%08x\n", data);
-            break;
-        case REG_Timer2Value:
-            //TODO: not implemented
-            System.out.printf("Timer2Value: 0x%08x\n", data);
-            break;
-        case REG_Timer2Control:
-            changeControl(1, regaddr, data);
-            break;
-        case REG_Timer2IntClr:
-            rawInt[1] &= ~data;
-            break;
-        case REG_Timer2RIS:
-        case REG_Timer2MIS:
-            //read only, ignored
-            break;
-        case REG_TimerPeriphID0:
-        case REG_TimerPeriphID1:
-        case REG_TimerPeriphID2:
-        case REG_TimerPeriphID3:
-        case REG_TimerPCellID0:
-        case REG_TimerPCellID1:
-        case REG_TimerPCellID2:
-        case REG_TimerPCellID3:
-            //read only, ignored
-            break;
-        default:
-            super.writeWord(regaddr, data);
-            break;
+        synchronized(this) {
+            switch (regaddr) {
+            case REG_Timer1Load:
+                loadValue[0] = data;
+                currentValue[0] = 0;
+                super.writeWord(regaddr, data);
+                break;
+            case REG_Timer1Value:
+                //read only, ignored
+                break;
+            case REG_Timer1Control:
+                updateControl(0, regaddr, data);
+                break;
+            case REG_Timer1IntClr:
+                rawInt[0] &= ~data;
+                break;
+            case REG_Timer1RIS:
+            case REG_Timer1MIS:
+                //read only, ignored
+                break;
+            case REG_Timer1BGLoad:
+                loadValue[0] = data;
+                super.writeWord(regaddr, data);
+                break;
+            case REG_Timer2Load:
+                loadValue[1] = data;
+                currentValue[1] = 0;
+                super.writeWord(regaddr, data);
+                break;
+            case REG_Timer2Value:
+                //read only, ignored
+                break;
+            case REG_Timer2Control:
+                updateControl(1, regaddr, data);
+                break;
+            case REG_Timer2IntClr:
+                rawInt[1] &= ~data;
+                break;
+            case REG_Timer2RIS:
+            case REG_Timer2MIS:
+                //read only, ignored
+                break;
+            case REG_Timer2BGLoad:
+                loadValue[1] = data;
+                super.writeWord(regaddr, data);
+                break;
+            case REG_TimerPeriphID0:
+            case REG_TimerPeriphID1:
+            case REG_TimerPeriphID2:
+            case REG_TimerPeriphID3:
+            case REG_TimerPCellID0:
+            case REG_TimerPCellID1:
+            case REG_TimerPCellID2:
+            case REG_TimerPCellID3:
+                //read only, ignored
+                break;
+            default:
+                super.writeWord(regaddr, data);
+                break;
+            }
         }
     }
 
@@ -209,8 +299,8 @@ public class DualTimer extends Controller64Reg32
 
     @Override
     public boolean isAssert() {
-        return (rawInt[0] & intMask[0]) != 0 ||
-                (rawInt[1] & intMask[1]) != 0;
+        return (rawInt[0] & intEnable[0]) != 0 ||
+                (rawInt[1] & intEnable[1]) != 0;
     }
 
     @Override
@@ -221,12 +311,34 @@ public class DualTimer extends Controller64Reg32
     @Override
     public void run() {
         while (!shouldHalt()) {
-            try {
-                Thread.sleep(4);
-                rawInt[0] = 0x1;
-                rawInt[1] = 0x1;
+            //FIXME: 100Hz polling
+            int hz = 100;
 
-                intDst.setRaisedInterrupt(isAssert());
+            try {
+                Thread.sleep(1000 / hz);
+
+                synchronized(this) {
+                    for (int id = 0; id < 2; id++) {
+                        if (!timerEn[id]) {
+                            //hold value
+                            continue;
+                        }
+
+                        int dec = clock / (hz * prescale[id]);
+                        int after = currentValue[id] - dec;
+
+                        if (currentValue[id] == 0 ||
+                                after == 0 || after > currentValue[id]) {
+                            //0 または 0 以下に達した
+                            currentValue[id] = loadValue[id];
+                            rawInt[id] = 0x1;
+                        } else {
+                            currentValue[id] = after;
+                        }
+                    }
+
+                    intDst.setRaisedInterrupt(isAssert());
+                }
             } catch (InterruptedException e) {
                 //ignore
             }
