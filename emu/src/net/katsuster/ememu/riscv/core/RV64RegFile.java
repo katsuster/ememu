da65c9777683423d9eb948c2f362303395be9d29
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-06-14 21:52:46 +0900

    riscv: fix register name

--- emu/src/net/katsuster/ememu/riscv/core/RV64RegFile.java
+++ emu/src/net/katsuster/ememu/riscv/core/RV64RegFile.java
@@ -15,10 +15,10 @@ public class RV64RegFile implements Reg64File {
 
     public RV64RegFile() {
         String[] name_usr = {
-                "r0",  "r1",  "r2",  "r3",  "r4",  "r5",  "r6",  "r7",
-                "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
-                "r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
-                "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
+                "zero", "ra",  "sp",  "gp",  "tp",  "t0",  "t1",  "t2",
+                "fp",   "s1",  "a0",  "a1",  "a2",  "a3",  "a4",  "a5",
+                "a6",   "a7",  "s2",  "s3",  "s4",  "s5",  "s6",  "s7",
+                "s8",   "s9",  "s10", "s11", "t3",  "t4",  "t5",  "t6",
                 "pc",
         };
 
