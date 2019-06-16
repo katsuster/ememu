d9728cffbd08e488c22f9bb987fab527f398523a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-06-16 15:22:10 +0900

    test: add test for Bus class

--- emu/src/net/katsuster/ememu/test/AllTest.java
+++ emu/src/net/katsuster/ememu/test/AllTest.java
@@ -11,6 +11,7 @@ import org.junit.runners.Suite.SuiteClasses;
         IntegerExtTest.class,
         SlaveCoreTest.class,
         RAMTest.class,
+        BusTest.class,
 })
 public class AllTest {
     protected AllTest() {
---
+++ emu/src/net/katsuster/ememu/test/BusTest.java
@@ -0,0 +1,106 @@
+package net.katsuster.ememu.test;
+
+import net.katsuster.ememu.generic.*;
+import org.junit.*;
+
+public class BusTest {
+    @Test
+    public void testAddSlaveCore() throws Exception {
+        String msg1 = "addSlaveCore() failed.";
+        String msg2 = "addSlaveCore() address check failed.";
+        String msg3 = "addSlaveCore() duplicate check failed.";
+        String msg4 = "addSlaveCore() null check failed.";
+
+        Bus bus = new Bus();
+        RAM16 ram1 = new RAM16(0x1000);
+        RAM32 ram2 = new RAM32(0x1000);
+        RAM64 ram3 = new RAM64(0x1000);
+
+        try {
+            bus.addSlaveCore(ram1, 0x0, 0xfff);
+            bus.addSlaveCore(ram2, 0x8000, 0x8fff);
+            bus.addSlaveCore(ram3, 0x10000, 0x10fff);
+
+            //mirror
+            bus.addSlaveCore(ram1, 0x2000, 0x2fff);
+
+            //over 32bit
+            bus.addSlaveCore(ram1, 0x100000000L, 0x1ffffffffL);
+        } catch (Exception e) {
+            Assert.fail(msg1);
+        }
+
+        try {
+            //wrong range
+            bus.addSlaveCore(ram1, 0x1000, 0xff);
+            Assert.fail(msg2);
+        } catch (Exception e) {
+            //OK
+        }
+
+        try {
+            //duplicate
+            bus.addSlaveCore(ram1, 0x0, 0xfff);
+            Assert.fail(msg3);
+        } catch (Exception e) {
+            //OK
+        }
+
+        try {
+            //null
+            bus.addSlaveCore(null, 0x0, 0xfff);
+            Assert.fail(msg4);
+        } catch (Exception e) {
+            //OK
+        }
+    }
+
+    @Test
+    public void testRemoveSlaveCore() throws Exception {
+        String msg1 = "removeSlaveCore() failed.";
+        String msg2 = "removeSlaveCore() re-add failed.";
+        String msg3 = "removeSlaveCore(null) ignore failed.";
+
+        Bus bus = new Bus();
+        RAM16 ram1 = new RAM16(0x1000);
+        RAM32 ram2 = new RAM32(0x1000);
+        RAM64 ram3 = new RAM64(0x1000);
+        boolean result;
+
+        bus.addSlaveCore(ram1, 0x0, 0xfff);
+        bus.addSlaveCore(ram1, 0x2000, 0x2fff);
+        bus.addSlaveCore(ram2, 0x8000, 0x8fff);
+        bus.addSlaveCore(ram3, 0x10000, 0x10fff);
+
+        try {
+            //simple
+            result = bus.removeSlaveCore(ram1);
+            Assert.assertTrue(msg1, result);
+            result = bus.removeSlaveCore(ram2);
+            Assert.assertTrue(msg1, result);
+            result = bus.removeSlaveCore(ram3);
+            Assert.assertTrue(msg1, result);
+
+            //non exist
+            result = bus.removeSlaveCore(ram1);
+            Assert.assertFalse(msg1, result);
+        } catch (Exception e) {
+            Assert.fail(msg1);
+        }
+
+        try {
+            //re-add
+            bus.addSlaveCore(ram1, 0x0, 0xfff);
+            bus.addSlaveCore(ram1, 0x20000, 0x20fff);
+        } catch (Exception e) {
+            Assert.fail(msg2);
+        }
+
+        try {
+            result = bus.removeSlaveCore(null);
+            Assert.assertFalse(msg1, result);
+        } catch (Exception e) {
+            Assert.fail(msg1);
+        }
+    }
+}
