1654a5116fe40f57fb4fc02ec92dfe6d0bf05083
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-09-12 22:35:06 +0900

    riscv: add hartid register

---
+++ emu/src/net/katsuster/ememu/riscv/core/reg/RegHartid64.java
@@ -0,0 +1,24 @@
+package net.katsuster.ememu.riscv.core.reg;
+
+import net.katsuster.ememu.generic.*;
+import net.katsuster.ememu.riscv.core.*;
+
+public class RegHartid64 extends Reg64 {
+    private RV64 core;
+
+    public RegHartid64(String name, long val, RV64 c) {
+        super(name, 0);
+
+        core = c;
+    }
+
+    @Override
+    public long getValue() {
+        return core.getThreadID();
+    }
+
+    @Override
+    public void setValue(long v) {
+        // Ignore
+    }
+}
