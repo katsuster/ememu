1d413dcd8ad85be252eff806dae767c4a08d819d
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-09-16 01:10:32 +0900

    riscv: add PRCI

--- emu/src/net/katsuster/ememu/board/RISCVUnleashed.java
+++ emu/src/net/katsuster/ememu/board/RISCVUnleashed.java
@@ -62,6 +62,7 @@ public class RISCVUnleashed extends AbstractBoard {
         CLINT clint = new CLINT(cpu);
         RAM l2lim = new RAM32(32 * 1024 * 1024);
         cl0_ddr = new RAM32(64 * 1024 * 1024);
+        PRCI prci = new PRCI(cpu);
 
         //Master core
         for (int i = 0; i < cpu.length; i++) {
@@ -75,10 +76,12 @@ public class RISCVUnleashed extends AbstractBoard {
         //  0x0000_1000 - 0x0000_1fff: Mode Select
         //  0x0001_0000 - 0x0001_7fff: Mask ROM
         //  0x0800_0000 - 0x09ff_ffff: L2 LIM
+        //  0x1000_0000 - 0x1000_0fff: L2 LIM
         bus.addSlaveCore(mode_select, 0x00001000L, 0x00001fffL);
         bus.addSlaveCore(mask_rom, 0x00010000L, 0x00017fffL);
         bus.addSlaveCore(clint.getSlaveCore(), 0x02000000L, 0x0200ffffL);
         bus.addSlaveCore(l2lim, 0x08000000L, 0x09ffffffL);
+        bus.addSlaveCore(prci.getSlaveCore(), 0x10000000L, 0x10000fffL);
 
         //reset CPU
         for (int i = 0; i < cpu.length; i++) {
---
+++ emu/src/net/katsuster/ememu/riscv/PRCI.java
@@ -0,0 +1,81 @@
+package net.katsuster.ememu.riscv;
+
+import net.katsuster.ememu.generic.*;
+import net.katsuster.ememu.riscv.core.*;
+
+/**
+ * Power Reset Clocking Interrupt (PRCI)
+ *
+ * 参考: SiFive FU540-C000 Manual: v1p0
+ */
+public class PRCI implements ParentCore {
+    private RV64[] cores;
+    private PRCI.PRCISlave slave;
+
+    public static final int REG_HFXOSCCFG         = 0x0000;
+    public static final int REG_COREPLLCFG0       = 0x0004;
+    public static final int REG_DDRPLLCFG0        = 0x000c;
+    public static final int REG_DDRPLLCFG1        = 0x0010;
+    public static final int REG_GEMGXLPLLCFG0     = 0x001c;
+    public static final int REG_GEMGXLPLLCFG1     = 0x0020;
+    public static final int REG_CORECLKSEL        = 0x0024;
+    public static final int REG_DEVICESRESETREG   = 0x0028;
+
+    public static final int REG_UNDOCUMENTD0      = 0x002c;
+
+    public PRCI(RV64[] c) {
+        cores = c;
+        slave = new PRCISlave();
+    }
+
+    @Override
+    public SlaveCore64 getSlaveCore() {
+        return slave;
+    }
+
+    class PRCISlave extends Controller32 {
+        public PRCISlave() {
+            addReg(REG_HFXOSCCFG,       "HFXOSCCFG", 0x00000000);
+            addReg(REG_COREPLLCFG0,     "COREPLLCFG0", 0x00000000);
+            addReg(REG_DDRPLLCFG0,      "DDRPLLCFG0", 0x00000000);
+            addReg(REG_DDRPLLCFG1,      "DDRPLLCFG1", 0x00000000);
+            addReg(REG_GEMGXLPLLCFG0,   "GEMGXLPLLCFG0", 0x00000000);
+            addReg(REG_GEMGXLPLLCFG1,   "GEMGXLPLLCFG1", 0x00000000);
+            addReg(REG_CORECLKSEL,      "CORECLKSEL", 0x00000000);
+            addReg(REG_DEVICESRESETREG, "DEVICESRESETREG", 0x00000000);
+
+            addReg(REG_UNDOCUMENTD0, "UNDOCUMENTED0", 0x00000000);
+        }
+        @Override
+        public int readWord(BusMaster64 m, long addr) {
+            int regaddr;
+            int result;
+
+            regaddr = (int) (addr & BitOp.getAddressMask(LEN_WORD_BITS));
+
+            switch (regaddr) {
+            default:
+                result = super.readWord(m, regaddr);
+                break;
+            }
+
+            return result;
+        }
+
+        @Override
+        public void writeWord(BusMaster64 m, long addr, int data) {
+            int regaddr;
+
+            regaddr = (int) (addr & BitOp.getAddressMask(LEN_WORD_BITS));
+
+            switch (regaddr) {
+            case REG_UNDOCUMENTD0:
+                System.out.printf("prci: UNDOCUMENTED0: %08x\n", data);
+                break;
+            default:
+                super.writeWord(m, regaddr, data);
+                break;
+            }
+        }
+    }
+}
