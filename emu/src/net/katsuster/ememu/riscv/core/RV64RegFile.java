729a456ac9d714e10ce01bd7b53b4686ca0a030d
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-09-30 05:42:01 +0900

    riscv: change name of x8 reg from fp to s0 same as gas

--- emu/src/net/katsuster/ememu/riscv/core/RV64RegFile.java
+++ emu/src/net/katsuster/ememu/riscv/core/RV64RegFile.java
@@ -17,7 +17,7 @@ public class RV64RegFile implements Reg64File {
     public RV64RegFile() {
         String[] name_usr = {
                 "zero", "ra",  "sp",  "gp",  "tp",  "t0",  "t1",  "t2",
-                "fp",   "s1",  "a0",  "a1",  "a2",  "a3",  "a4",  "a5",
+                "s0",   "s1",  "a0",  "a1",  "a2",  "a3",  "a4",  "a5",
                 "a6",   "a7",  "s2",  "s3",  "s4",  "s5",  "s6",  "s7",
                 "s8",   "s9",  "s10", "s11", "t3",  "t4",  "t5",  "t6",
                 "pc",
