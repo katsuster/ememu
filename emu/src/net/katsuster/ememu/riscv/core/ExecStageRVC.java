4b3406a8f884aa599e79dbab66ef5f8fcf5aebea
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-09-30 02:11:59 +0900

    riscv: add c.or instruction

--- emu/src/net/katsuster/ememu/riscv/core/ExecStageRVC.java
+++ emu/src/net/katsuster/ememu/riscv/core/ExecStageRVC.java
@@ -278,6 +278,26 @@ public class ExecStageRVC extends Stage64 {
     }
 
     /**
+     * OR (Or) 命令。
+     *
+     * @param inst 16bit 命令
+     * @param exec デコードと実行なら true、デコードのみなら false
+     */
+    public void executeOr(InstructionRV16 inst, boolean exec) {
+        int rd = inst.getRs1dash() + 8;
+        int rs2 = inst.getRs2dash() + 8;
+
+        if (!exec) {
+            printDisasm(inst, "c.or",
+                    String.format("%s, %s, %s", getRegName(rd),
+                            getRegName(rd), getRegName(rs2)));
+            return;
+        }
+
+        setReg(rd, getReg(rd) | getReg(rs2));
+    }
+
+    /**
      * ADDW (Add word) 命令。
      *
      * @param inst 16bit 命令
@@ -549,6 +569,9 @@ public class ExecStageRVC extends Stage64 {
             break;
         case INS_RVC_ANDI:
             executeAndi(inst, exec);
+            break;
+        case INS_RVC_OR:
+            executeOr(inst, exec);
             break;
         case INS_RVC_ADDW:
             executeAddw(inst, exec);
