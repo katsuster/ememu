3c8cd2cab03decaf0e71114d0b45c78bfcbdc87b
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-10-07 23:39:25 +0900

    riscv: add subw instruction

--- emu/src/net/katsuster/ememu/riscv/core/ExecStageRVI.java
+++ emu/src/net/katsuster/ememu/riscv/core/ExecStageRVI.java
@@ -861,6 +861,29 @@ public class ExecStageRVI extends Stage64 {
     }
 
     /**
+     * SUBW (subtract word) 命令。
+     *
+     * @param inst 32bit 命令
+     * @param exec デコードと実行なら true、デコードのみなら false
+     */
+    public void executeSubw(InstructionRV32 inst, boolean exec) {
+        int rd = inst.getRd();
+        int rs1 = inst.getRs1();
+        int rs2 = inst.getRs2();
+        long v;
+
+        if (!exec) {
+            printDisasm(inst, "subw",
+                    String.format("%s, %s, %s", getRegName(rd),
+                            getRegName(rs1), getRegName(rs2)));
+            return;
+        }
+
+        v = (getReg(rs1) - getReg(rs2)) & 0xffffffffL;
+        setReg(rd, BitOp.signExt64(v & 0xffffffffL, 32));
+    }
+
+    /**
      * SRLW (shift right logical word) 命令。
      *
      * @param inst 32bit 命令
@@ -1178,6 +1201,9 @@ public class ExecStageRVI extends Stage64 {
             break;
         case INS_RV64I_ADDIW:
             executeAddiw(inst, exec);
+            break;
+        case INS_RV64I_SUBW:
+            executeSubw(inst, exec);
             break;
         case INS_RV64I_SRLW:
             executeSrlw(inst, exec);
