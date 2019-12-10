faf1d9895baaf408cc4562332e6f2a46e426c963
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2019-12-11 02:11:43 +0900

    riscv: implement minimum features of SPI core

--- emu/src/net/katsuster/ememu/riscv/SPI.java
+++ emu/src/net/katsuster/ememu/riscv/SPI.java
@@ -34,20 +34,168 @@ public class SPI extends AbstractParentCore {
         mc = new SPIMaster();
         setMasterCore(mc);
 
-        sc = new SPISlave();
+        sc = new SPISlave(this);
         setSlaveCore(sc);
     }
 
     class SPIMaster extends MasterCore64 {
-        public SPIMaster() {
+        private int select = 0;
+        private boolean enableSelect = true;
+        private byte[] txFifo;
+        private byte[] rxFifo;
+        private int lenFifo;
+        private int rdTx = 0, wrTx = 0, lenTx = 0;
+        private int rdRx = 0, wrRx = 0, lenRx = 0;
+        private final Object obj;
 
+        public SPIMaster() {
+            lenFifo = 8;
+            txFifo = new byte[lenFifo];
+            rxFifo = new byte[lenFifo];
+            obj = new Object();
+        }
+
+        public int getChipSelect() {
+            return select;
+        }
+
+        public void setChipSelect(int s) {
+            select = s;
+        }
+
+        public boolean getEnableChipSelect() {
+            return enableSelect;
+        }
+
+        public void setEnableChipSelect(boolean b) {
+            enableSelect = b;
+        }
+
+        public boolean isTxFull() {
+            return lenTx == lenFifo;
+        }
+
+        public boolean isTxEmpty() {
+            return lenTx == 0;
+        }
+
+        public void pushTx(byte b) {
+            synchronized (obj) {
+                if (isTxFull()) {
+                    throw new IllegalStateException("Tx FIFO is full, cannot push");
+                }
+
+                txFifo[wrTx] = b;
+                wrTx++;
+                wrTx %= lenFifo;
+                lenTx++;
+                obj.notifyAll();
+            }
+        }
+
+        public byte popTx() {
+            synchronized (obj) {
+                if (isTxEmpty()) {
+                    throw new IllegalStateException("Tx FIFO is empty, cannot pop");
+                }
+
+                byte b = txFifo[rdTx];
+                rdTx++;
+                rdTx %= lenFifo;
+                lenTx--;
+                obj.notifyAll();
+                return b;
+            }
+        }
+
+        public boolean isRxFull() {
+            return lenRx == lenFifo;
+        }
+
+        public boolean isRxEmpty() {
+            return lenRx == 0;
+        }
+
+        public void pushRx(byte b) {
+            synchronized (obj) {
+                if (isRxFull()) {
+                    throw new IllegalStateException("Rx FIFO is full, cannot push");
+                }
+
+                rxFifo[wrRx] = b;
+                wrRx++;
+                wrRx %= lenFifo;
+                lenRx++;
+                obj.notifyAll();
+            }
+        }
+
+        public byte popRx() {
+            synchronized (obj) {
+                if (isRxEmpty()) {
+                    throw new IllegalStateException("Rx FIFO is empty, cannot pop");
+                }
+
+                byte b = rxFifo[rdRx];
+                rdRx++;
+                rdRx %= lenFifo;
+                lenRx--;
+                obj.notifyAll();
+                return b;
+            }
+        }
+
+        private void process() throws InterruptedException {
+            synchronized (obj) {
+                while (isTxEmpty()) {
+                    obj.wait();
+                    if (shouldHalt()) {
+                        return;
+                    }
+                }
+
+                if (!enableSelect) {
+                    //not chip select
+                    popTx();
+                    pushRx((byte)0xff);
+                    return;
+                }
+
+                SlaveCore64 sc = getSlaveBus().getSlaveCore(select, select);
+                if (sc == null) {
+                    //not connected
+                    popTx();
+                    pushRx((byte)0xff);
+                } else {
+                    write8(select, popTx());
+                    byte b = read8(select);
+                    pushRx(b);
+                }
+            }
+        }
+
+        public final Object getSync() {
+            return obj;
+        }
+
+        @Override
+        public void run() {
+            while (!shouldHalt()) {
+                try {
+                    process();
+                } catch (InterruptedException e) {
+                    //ignored
+                }
+            }
         }
     }
 
     class SPISlave extends Controller32 {
-        private int cs = 0;
+        private SPI parent;
 
-        public SPISlave() {
+        public SPISlave(SPI p) {
+            parent = p;
+
             addReg(REG_SCKDIV,  "SCKDIV", 0x00000000);
             /*
             addReg(REG_SCKMODE, "SCKMODE", 0x00000000);
@@ -77,30 +225,38 @@ public class SPI extends AbstractParentCore {
         @Override
         public int readWord(BusMaster64 m, long addr) {
             int regaddr;
-            int result;
+            int result = 0;
 
             regaddr = (int) (addr & BitOp.getAddressMask(LEN_WORD_BITS));
 
             switch (regaddr) {
             case REG_CSID:
-                result = cs;
-                System.out.printf("SPI CSID: read 0x%x\n", result);
+                result = mc.getChipSelect();
+                System.out.printf("SPI(%s) CSID: read 0x%x\n", parent.getName(), result);
                 break;
             case REG_CSDEF:
-                result = 0;
-                System.out.printf("SPI CSDEF: read 0x%x\n", result);
+                System.out.printf("SPI(%s) CSDEF: read 0x%x\n", parent.getName(), result);
                 break;
             case REG_CSMODE:
-                result = 0;
-                System.out.printf("SPI CSMODE: read 0x%x\n", result);
+                System.out.printf("SPI(%s) CSMODE: read 0x%x\n", parent.getName(), result);
                 break;
             case REG_TXDATA:
-                result = 0;
-                System.out.printf("SPI TXDATA: read 0x%x\n", result);
+                synchronized (mc.getSync()) {
+                    if (mc.isTxFull()) {
+                        result |= 0x80000000;
+                    }
+                }
+                //System.out.printf("SPI(%s) TXDATA: read 0x%x\n", parent.getName(), result);
                 break;
             case REG_RXDATA:
-                result = 0;
-                System.out.printf("SPI RXDATA: read 0x%x\n", result);
+                synchronized (mc.getSync()) {
+                    if (mc.isRxEmpty()) {
+                        result |= 0x80000000;
+                    } else {
+                        result |= mc.popRx() & 0xff;
+                        System.out.printf("SPI(%s) RXDATA: read 0x%x\n", parent.getName(), result);
+                    }
+                }
                 break;
             default:
                 result = super.readWord(m, regaddr);
@@ -118,29 +274,55 @@ public class SPI extends AbstractParentCore {
 
             switch (regaddr) {
             case REG_SCKDIV:
-                System.out.printf("SPI SCKDIV: write 0x%x\n", data);
+                System.out.printf("SPI(%s) SCKDIV: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_CSID:
-                cs = data;
-                System.out.printf("SPI CSID: write 0x%x\n", data);
+                mc.setChipSelect(data);
+                System.out.printf("SPI(%s) CSID: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_CSDEF:
-                System.out.printf("SPI CSDEF: write 0x%x\n", data);
+                System.out.printf("SPI(%s) CSDEF: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_CSMODE:
-                System.out.printf("SPI CSMODE: write 0x%x\n", data);
+                int mode = BitOp.getField32(data, 0, 2);
+
+                //Chip select mode
+                switch (mode) {
+                case 0:
+                case 2:
+                    //AUTO, HOLD
+                    mc.setEnableChipSelect(true);
+                    break;
+                case 3:
+                    //OFF
+                    mc.setEnableChipSelect(false);
+                    break;
+                }
+
+                System.out.printf("SPI(%s) CSMODE: write 0x%x\n" +
+                                "  %s: 0x%x, \n",
+                        parent.getName(), data,
+                        "mode", mode);
+
+                super.writeWord(m, regaddr, data);
+
                 break;
             case REG_FMT:
-                System.out.printf("SPI FMT: write 0x%x\n", data);
+                System.out.printf("SPI(%s) FMT: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_TXDATA:
-                System.out.printf("SPI TXDATA: write 0x%x\n", data);
+                synchronized (mc.getSync()) {
+                    if (!mc.isTxFull()) {
+                        mc.pushTx((byte)data);
+                        System.out.printf("SPI(%s) TXDATA: write 0x%x\n", parent.getName(), data);
+                    }
+                }
                 break;
             case REG_RXDATA:
-                System.out.printf("SPI RXDATA: write 0x%x\n", data);
+                System.out.printf("SPI(%s) RXDATA: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_FCTRL:
-                System.out.printf("SPI FCTRL: write 0x%x\n", data);
+                System.out.printf("SPI(%s) FCTRL: write 0x%x\n", parent.getName(), data);
                 break;
             case REG_FFMT:
                 int cmd_en = BitOp.getField32(data, 0, 1);
@@ -152,7 +334,7 @@ public class SPI extends AbstractParentCore {
                 int cmd_code = BitOp.getField32(data, 16, 8);
                 int pad_code = BitOp.getField32(data, 24, 8);
 
-                System.out.printf("SPI FFMT: write 0x%x\n" +
+                System.out.printf("SPI(%s) FFMT: write 0x%x\n" +
                                 "  %s: 0x%x, \n" +
                                 "  %s: 0x%x, \n" +
                                 "  %s: 0x%x, \n" +
@@ -161,7 +343,7 @@ public class SPI extends AbstractParentCore {
                                 "  %s: 0x%x, \n" +
                                 "  %s: 0x%x, \n" +
                                 "  %s: 0x%x, \n",
-                        data,
+                        parent.getName(), data,
                         "cmd_en", cmd_en,
                         "addr_len", addr_len,
                         "pad_cnt", pad_cnt,
